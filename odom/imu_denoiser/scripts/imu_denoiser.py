068a1aaff51f34ec27c995a566e4b677748096b1
Ivy <ivtn0912@gmail.com>
2025-05-11 18:22:17 -0400

    Updating odom configs, added spatial_imu.launch

--- odom/imu_denoiser/scripts/imu_denoiser.py
+++ odom/imu_denoiser/scripts/imu_denoiser.py
@@ -80,6 +80,8 @@ class ButterworthIMUDenoiser:
             current_val = getattr(lin, axis)
             filt_val, new_state = self.update_filter(current_val, self.state_lin[axis])
             self.state_lin[axis] = new_state
+            if axis == 'x':
+                filt_val+=0.7
             f_lin[axis] = filt_val
 
         filtered_msg.angular_velocity = Vector3(**f_ang)
