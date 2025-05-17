cb5929686bb98fab3f202cf107dae60ba5d301bf
Hari Om Chadha <chadhahariom1@gmail.com>
2025-05-17 17:27:13 -0400

    updated the urdf file to get a better relative position

--- odom/imu_denoiser/scripts/imu_denoiser.py
+++ odom/imu_denoiser/scripts/imu_denoiser.py
@@ -80,8 +80,8 @@ class ButterworthIMUDenoiser:
             current_val = getattr(lin, axis)
             filt_val, new_state = self.update_filter(current_val, self.state_lin[axis])
             self.state_lin[axis] = new_state
-            if axis == 'x':
-                filt_val+=0.7
+            # if axis == 'x':
+            #     filt_val+=0.7
             if abs(filt_val)<0.1:
                 filt_val = 0
             f_lin[axis] = filt_val
