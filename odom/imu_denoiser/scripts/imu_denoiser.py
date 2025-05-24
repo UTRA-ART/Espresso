f52d2f126348b1e4ae23329d419a9301c9e9e21a
luotiger <114829395+luotiger@users.noreply.github.com>
2025-05-24 04:02:49 -0400

    Matching imu_denoiser.py to primary_laptop

--- odom/imu_denoiser/scripts/imu_denoiser.py
+++ odom/imu_denoiser/scripts/imu_denoiser.py
@@ -80,8 +80,14 @@ class ButterworthIMUDenoiser:
             current_val = getattr(lin, axis)
             filt_val, new_state = self.update_filter(current_val, self.state_lin[axis])
             self.state_lin[axis] = new_state
+            # Adding manual offsets for constant accels on the IMU. Adjust every time to touch/move the IMU
+            # Unused rn because 
             if axis == 'x':
-                filt_val+=0.7
+                filt_val+=0
+            if axis == 'z':
+                filt_val=0
+            if axis == 'y':
+                filt_val+=0
             if abs(filt_val)<0.1:
                 filt_val = 0
             f_lin[axis] = filt_val
