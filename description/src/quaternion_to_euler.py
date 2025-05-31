ca3b135c780f265c4259c8261b19a7a2175a7156
Ivy <ivtn0912@gmail.com>
2025-05-31 13:41:50 -0400

    Testing modifications

--- description/src/quaternion_to_euler.py
+++ description/src/quaternion_to_euler.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
-def imu_callback(msg):
-    # Extract quaternion
-    q = msg.orientation
+def odom_callback(msg):
+    # Extract quaternion from Odometry pose
+    q = msg.pose.pose.orientation
     quaternion = [q.x, q.y, q.z, q.w]
     
     # Convert to Euler angles
     roll, pitch, yaw = euler_from_quaternion(quaternion)
+    yaw = yaw*180/3.1415
 
     # Log the results
     rospy.loginfo("Roll: %.3f, Pitch: %.3f, Yaw: %.3f", roll, pitch, yaw)
 
 def quaternion_to_euler_node():
     rospy.init_node('quaternion_to_euler_node', anonymous=True)
-    rospy.Subscriber("/imu/data", Imu, imu_callback)
+    rospy.Subscriber("/zed_node/odom", Odometry, odom_callback)
     rospy.spin()
 
 if __name__ == '__main__':
@@ -25,3 +26,4 @@ if __name__ == '__main__':
         quaternion_to_euler_node()
     except rospy.ROSInterruptException:
         pass
+
